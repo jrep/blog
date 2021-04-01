8c18767be7892b4988057ce61cfbaffc6584dd09
Jack Repenning <Repenning.Jack@gmail.com>
2021-03-31 18:10:44 -0700

    Create article entry works

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "This is not the title", body: "The body. Or, something like it.")
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
 end
